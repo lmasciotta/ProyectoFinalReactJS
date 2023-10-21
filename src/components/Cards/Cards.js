675a382df9b91f79b830ced3d91f50b75fcd5e8f
lucasm <lmasciotta87@gmail.com>
2023-10-20 22:28:19 -0300

    first commit

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -5,15 +5,14 @@ import { Link } from 'react-router-dom';
 export default function Cards({ modelo, color, precio, detalle, id, categoria }) {
   const imagenProducto = require(`../../assets/${modelo}.jpg`);
 
-
   return (
-    <div className='Card card'> 
-      <img src={imagenProducto} className="imagen card-img-top" alt={modelo} /> 
+    <div className='Cards'>
+      <img src={imagenProducto} className="imagen card-img-top" alt={modelo} />
       <div className="card-body">
-        <h2 className="card-title">{modelo}</h2> 
-        <p className="card-text">{color}</p> 
+        <h2 className="card-title">{modelo}</h2>
+        <p className="card-text">{color}</p>
         <p className="card-text">{precio}</p>
-        <Link to={`/${categoria}/Details/${id}`} className="btn btn-warning" >Ver Detalle</Link>
+        <Link to={`/${categoria}/Details/${id}`} className="ver-detalle-link">Ver Detalle</Link>
       </div>
     </div>
   );
--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,33 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useContext } from 'react';
 import { CartContext } from '../Context/CartContext';
 import { useForm } from 'react-hook-form'; 
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../Firebase/config.js';
+import { useState } from 'react';
 import './style.css';
 
 const Checkout = () => {
-    const [pedidoID, setPedidoId] = useState("");
-    const [procesando, setProcesando] = useState(false); 
+    const [pedidoID, setPedidoId] = useState("")
     const { carrito, precioTotal, vaciarCarrito } = useContext(CartContext);
     const { register, handleSubmit } = useForm(); 
+    const [procesando, setProcesando] = useState(false); 
 
     const comprar = (data) => {
+        
         setProcesando(true);
+        setTimeout(() => {
+            const pedido = {
+                cliente: data,
+                productos: carrito,
+                total: precioTotal(),
+            };
+            console.log(pedido);
 
-        const pedido = {
-            cliente: data,
-            productos: carrito,
-            total: precioTotal(),
-        };
+            const pedidosRef = collection(db, "pedidos");
 
-        const pedidosRef = collection(db, "pedidos");
-
-        addDoc(pedidosRef, pedido)
-        .then((doc) => {
-            setPedidoId(doc.id);
-            setProcesando(false); 
-        });
+            addDoc(pedidosRef, pedido)
+                .then((doc) => {
+                    setPedidoId(doc.id);
+                    setProcesando(false); 
+                });
+        }, 3000); 
     }
 
     if (pedidoID) {
@@ -37,18 +41,18 @@ const Checkout = () => {
                 <p>Su número de pedido es: {pedidoID}</p>
                 <h2>Muchas gracias por tu compra!</h2>
             </div>
-        );
+        )
     }
 
     return (
-        <div className="Card">
+        <div className="Checkout">
             <h1 className="main-title">Finalizar Compra</h1>
             <form className="formulario" onSubmit={handleSubmit(comprar)}>
                 <input type="text" placeholder="Ingresá tu nombre" {...register("nombre")} />
                 <input type="email" placeholder="Ingresá tu e-mail" {...register("email")} />
                 <input type="tel" placeholder="Ingresá tu teléfono" {...register("telefono")} />
-                <button className="enviar" type="submit" disabled={procesando}>
-                    {procesando ? "Procesando" : "Comprar"}
+                <button className="enviar" type="submit">
+                    {procesando ? "Procesando..." : "Comprar"}
                 </button>
             </form>
         </div>
